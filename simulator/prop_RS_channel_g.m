6812d568d7cc308f1b032595aa16524213e05cce
Vahid Yazdnian Kashani <vy9053@della-yasaman.princeton.edu>
2024-12-19 09:12:39 -0500

    transfer_function

--- simulator/prop_RS_channel_g.m
+++ simulator/prop_RS_channel_g.m
@@ -1,11 +1,12 @@
 function H = prop_RS_channel_g(M, L,freq, z,theta)
 %     """Compute diffracted field with the Rayleigh-Sommerfeld propagation using
 %     the transfer function.
-    dx = L/M;
+    dx = L/(M-1);
     c =  physconst('LightSpeed');
     wlen = c./freq;
     k = (2*pi)/wlen;
-    fx=((1/(dx))*((-M/2:M/2-1)/M)).';
+    %fx=((1/(dx))*((-M/2:M/2-1)/M)).';
+    fx=((1/(dx))*((-(M-1)/2:(M-1)/2)/(M-1))).';
     H = exp(1i*k*z./cos(theta).*sqrt(1-(wlen.*cos(theta).*fx).^2));
-    H = fftshift(H);
+    H = ifftshift(H);
 end
